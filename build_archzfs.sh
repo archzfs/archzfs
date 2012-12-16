0f66de6fc6d1e5b15d6201394a5795a64073a3d9
Jesus Alvarez <jeezusjr@gmail.com>
2012-12-16 09:34:48 -0800

    build_archzfs: Move packages to backup directory

--- build_archzfs.sh
+++ build_archzfs.sh
@@ -3,15 +3,30 @@
 cd devsrc/spl-utils
 
 makepkg -sfic --noconfirm
+makepkg -Sfc
 
 cd ../spl
 
 makepkg -sfic --noconfirm
+makepkg -Sfc
 
 cd ../zfs-utils
 
 makepkg -sfic --noconfirm
+makepkg -Sfc
 
 cd ../zfs
 
 makepkg -sfic --noconfirm
+makepkg -Sfc
+
+cd ../../backup/latest
+
+find . -iname '*.src.tar*' -exec mv {} ../sources/ \;
+find . -iname '*.pkg.tar*' -exec mv {} ../packages/ \;
+
+cd ../../devsrc
+
+find . -iname '*.src.tar*' -exec mv {} ../backup/latest \;
+find . -iname '*.pkg.tar*' -exec mv {} ../backup/latest \;
+
