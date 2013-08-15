503d379532a9cfe2cba88fcdf723da1e194a8418
Jesus Alvarez <jeezusjr@gmail.com>
2013-08-15 12:28:52 -0700

    conf.sh: Remove duplicate variable

--- conf.sh
+++ conf.sh
@@ -7,5 +7,4 @@ REPO_BASEPATH="/data/pacman/repo"
 REMOTE_LOGIN="jalvarez@jalvarez.webfactional.com"
 GPG_SIGN_KEY='0EE7A126'
 CHROOT_PATH="/opt/chroot"
-REPO_BASE="/mnt/data/pacman/repo"
 CHROOT_COPYNAME="azfs"
