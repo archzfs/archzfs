27cd9c808c4e38d399df14ba786f2b5272c33d69
Jesus Alvarez <jeezusjr@gmail.com>
2013-08-20 12:27:07 -0700

    verifier.sh: Fix broken logic

--- verifier.sh
+++ verifier.sh
@@ -12,7 +12,7 @@ source "conf.sh"
 compute_local_repo_hash() {
     # $1: The repository to compute
     # Sets LOCAL_REPO_HASH
-    msg "Computing local $1 repository hashes..."
+    msg2 "Computing local $1 repository hashes..."
     LFILES=$(cd $REPO_BASEPATH; sha256sum $1/{x86_64,i686}/*)
     if [[ $DEBUG == "1" ]]; then
         msg2 "Repository hash list:"
@@ -25,7 +25,7 @@ compute_local_repo_hash() {
 compute_remote_repo_hash() {
     # $1: The repository to compute
     # Sets REMOTE_REPO_HASH
-    msg "Computing remote $1 repository hashes..."
+    msg2 "Computing remote $1 repository hashes..."
     RFILES=$(ssh $REMOTE_LOGIN "cd webapps/default; sha256sum $1/{x86_64,i686}/*")
     if [[ $DEBUG == "1" ]]; then
         msg2 "Repository hash list:"
@@ -37,23 +37,20 @@ compute_remote_repo_hash() {
 
 compare_repo() {
     # $1: The repo name to compare
-    if [[ $(compute_local_repo_hash $1) != $(compute_remote_repo_hash $1) ]]; then
+    compute_local_repo_hash $1
+    compute_remote_repo_hash $1
+    if [[ $REMOTE_REPO_HASH != $LOCAL_REPO_HASH ]]; then
         return 1
     fi
+    return 0
 }
 
-#
-# Check demz-repo-archiso
-#
-if [[ $(compare_repo "demz-repo-archiso") != 0 ]]; then
-    msg "The database is out of sync, sending notification..."
-    send_email "demz-repo-archiso is out of sync!" "demz-repo-archiso is not in sync!"
-fi
-
-#
-# Check demz-repo-core
-#
-if [[ $(compare_repo "demz-repo-core") != 0 ]]; then
-    msg "The database is out of sync, sending notification..."
-    send_email "demz-repo-core is out of sync!" "demz-repo-core is not in sync!"
-fi
+for REPO in 'demz-repo-archiso' 'demz-repo-core'; do
+    msg "Checking ${REPO}..."
+    compare_repo $REPO
+    if [[ $? != 0 ]]; then
+        error "The database is out of sync, sending notification..."
+        send_email "$REPO is out of sync!" "$REPO is not in sync!"
+    fi
+    msg2 "$REPO is in sync"
+done
