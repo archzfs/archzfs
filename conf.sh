12c4b9e37dc1240ed535ba2d32e492ae0cd3b7f5
Jesus Alvarez <jeezusjr@gmail.com>
2013-09-28 12:02:57 -0700

    Update to 0.6.2_3.11.2-1

--- conf.sh
+++ conf.sh
@@ -1,12 +1,12 @@
 # Version information
-PKGREL="2"  # The pkgrel of all the archzfs packages
+PKGREL="1"  # The pkgrel of all the archzfs packages
 
 # ZFSonLinux version
 ZOL_VERSION="0.6.2"
 
 # Linux version dependencies
-LINUX_VERSION="3.11.1"
-LINUX_PKGREL="2"
+LINUX_VERSION="3.11.2"
+LINUX_PKGREL="1"
 LINUX_VERSION_FULL="$LINUX_VERSION-$LINUX_PKGREL"
 LINUX_ARCHISO="3.10.10"
 
