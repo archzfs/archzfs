826013575472d510a99d1773e634ac5e26763e55
Jesus Alvarez <jeezusjr@gmail.com>
2013-12-26 19:40:13 -0800

    Update to 0.6.2_3.12.6-1

--- conf.sh
+++ conf.sh
@@ -5,7 +5,7 @@ PKGREL="1"  # The pkgrel of all the archzfs packages
 ZOL_VERSION="0.6.2"
 
 # Linux version dependencies
-LINUX_VERSION="3.12.3"
+LINUX_VERSION="3.12.6"
 LINUX_PKGREL="1" # The PKGREL must be increased if this value changes.
 LINUX_VERSION_FULL="$LINUX_VERSION-$LINUX_PKGREL"
 
