5d370e059f455fe54c2cc65ed7ee1d6b341d81b8
Jesus Alvarez <jeezusjr@gmail.com>
2014-01-12 00:59:04 -0800

    Update to 0.6.2_3.12.7-1

--- conf.sh
+++ conf.sh
@@ -5,7 +5,7 @@ PKGREL="1"  # The pkgrel of all the archzfs packages
 ZOL_VERSION="0.6.2"
 
 # Linux version dependencies
-LINUX_VERSION="3.12.6"
+LINUX_VERSION="3.12.7"
 LINUX_PKGREL="1" # The PKGREL must be increased if this value changes.
 LINUX_VERSION_FULL="$LINUX_VERSION-$LINUX_PKGREL"
 
@@ -14,8 +14,8 @@ PKG_VERSION="${ZOL_VERSION}_${LINUX_VERSION}"
 FULL_VERSION="$PKG_VERSION-$PKGREL"
 
 # Archiso Configuration
-LINUX_ARCHISO="3.12.1"
-LINUX_ARCHISO_PKGREL="2"
+LINUX_ARCHISO="3.12.6"
+LINUX_ARCHISO_PKGREL="1"
 ARCHISO_FULL_VERSION="${ZOL_VERSION}_${LINUX_ARCHISO}-$LINUX_ARCHISO_PKGREL"
 
 # Notification address
