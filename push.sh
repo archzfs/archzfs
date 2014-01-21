559f917415b30f1e88519123184e255e30cba4ca
Jesus Alvarez <jeezusjr@gmail.com>
2014-01-20 22:56:28 -0800

    push.sh: Comment out rst2html output.

--- push.sh
+++ push.sh
@@ -5,15 +5,17 @@
 # the archzfs package documentation.
 #
 
-source "lib.sh"
-source "conf.sh"
+source ./lib.sh
+source ./conf.sh
 
 msg "Pushing the package sources to AUR..."
-FILES=$(find . -iname "*${ZOL_VERSION}_${LINUX_VERSION}-$PKGREL*.src.tar.gz")
+
+FILES=$(find . -iname "*${AZB_ZOL_VERSION}_${AZB_LINUX_VERSION}-$AZB_PKGREL*.src.tar.gz")
+
 burp -c modules $FILES -v
 
 # Build the documentation and push it to the remote host
-msg "Building the documentation..."
-rst2html2 web_archzfs.rst > /tmp/archzfs_index.html
-msg2 "Pushing the documentation to the remote host..."
-scp /tmp/archzfs_index.html $REMOTE_LOGIN:webapps/default/archzfs/index.html
+# msg "Building the documentation..."
+# rst2html2 web_archzfs.rst > /tmp/archzfs_index.html
+# msg2 "Pushing the documentation to the remote host..."
+# scp /tmp/archzfs_index.html $REMOTE_LOGIN:webapps/default/archzfs/index.html
