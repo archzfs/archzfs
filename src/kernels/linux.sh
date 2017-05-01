81259f7587847e5b532dbca64e0073e7add33d95
Jesus Alvarez <jeezusjr@gmail.com>
2017-05-01 07:23:32 -0700

    Update std packages for kernel 4.10.13-1

--- src/kernels/linux.sh
+++ src/kernels/linux.sh
@@ -4,7 +4,7 @@ mode_desc="Select and use the packages for the default linux kernel"
 
 # Kernel versions for default ZFS packages
 pkgrel="1"
-kernel_version="4.10.11-1"
+kernel_version="4.10.13-1"
 
 # Kernel version for GIT packages
 pkgrel_git="${pkgrel}"
