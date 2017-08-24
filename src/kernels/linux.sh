7b7f2c070393eefebee26e86f155b886bd2574d6
Jesus Alvarez <jeezusjr@gmail.com>
2017-08-23 22:56:14 -0700

    Update std packages for kernel 4.12.8-2

--- src/kernels/linux.sh
+++ src/kernels/linux.sh
@@ -3,8 +3,8 @@ mode_name="std"
 mode_desc="Select and use the packages for the default linux kernel"
 
 # Kernel versions for default ZFS packages
-pkgrel="2"
-kernel_version="4.12.8-1"
+pkgrel="1"
+kernel_version="4.12.8-2"
 
 # Kernel version for GIT packages
 pkgrel_git="${pkgrel}"
