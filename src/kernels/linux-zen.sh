ce3cccbc097ef665201418b51e2204749fcc43d5
Jesus Alvarez <jeezusjr@gmail.com>
2017-08-25 12:00:35 -0700

    Update zen packages for kernel 4.12.9-1

--- src/kernels/linux-zen.sh
+++ src/kernels/linux-zen.sh
@@ -3,8 +3,8 @@ mode_name="zen"
 mode_desc="Select and use the packages for the linux-zen kernel"
 
 # Kernel versions for default ZFS packages
-pkgrel="2"
-kernel_version="4.12.8-2"
+pkgrel="1"
+kernel_version="4.12.9-1"
 
 # Kernel version for GIT packages
 pkgrel_git="${pkgrel}"
