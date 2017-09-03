23c975da7c0cd448d25071618ce8b8db3dd254f2
Jesus Alvarez <jeezusjr@gmail.com>
2017-09-03 10:30:06 -0700

    Update lts packages for kernel 4.9.47-1

--- src/kernels/linux-lts.sh
+++ src/kernels/linux-lts.sh
@@ -4,7 +4,7 @@ mode_desc="Select and use the packages for the linux-lts kernel"
 
 # Kernel versions for LTS packages
 pkgrel="1"
-kernel_version="4.9.46-1"
+kernel_version="4.9.47-1"
 
 # Kernel version for GIT packages
 pkgrel_git="${pkgrel}"
