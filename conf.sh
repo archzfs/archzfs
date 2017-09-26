db9b30ae715b6dd18e37016fd3285a18c696de1a
Jesus Alvarez <jeezusjr@gmail.com>
2017-09-26 13:24:33 -0700

    Update to zfs version 0.7.2

--- conf.sh
+++ conf.sh
@@ -1,9 +1,9 @@
 # ZFSonLinux stable version
-zol_version="0.7.1"
+zol_version="0.7.2"
 
 # The ZOL source hashes are from zfsonlinux.org
-zfs_src_hash="231b104979ddacfeb1889e1dec175337276e7b3b109d40656089744b5caf3ef6"
-spl_src_hash="e6a83dc50bc83a5ce6f20238da16fb941ab6090c419be8af8fc9223210f637fd"
+zfs_src_hash="f75f4d8bbb8241e3d06321b53914e53fa22d1ccc8be89819b578b46e5d3e5cf4"
+spl_src_hash="c22e410c661a29acfa16caee21b82f8bb166f5b6611ec56431cd9c172ab4729e"
 zfs_bash_completion_hash="b60214f70ffffb62ffe489cbfabd2e069d14ed2a391fac0e36f914238394b540"
 zfs_initcpio_install_hash="e33adabbe3f2f4866802c9d63c7810c7a42b4df2288d0cdd23376519b15b36e4"
 zfs_initcpio_hook_hash="b5f87d1d1d10443d8919125a4c139d5f4c579ca4433b2905ee826bb01defa56a"
