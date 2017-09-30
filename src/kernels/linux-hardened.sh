959458e15e5d8bf2e1045cb1e3002d065bca8c5f
Jesus Alvarez <jeezusjr@gmail.com>
2017-09-30 12:10:11 -0700

    Update lts and hardened packages

--- src/kernels/linux-hardened.sh
+++ src/kernels/linux-hardened.sh
@@ -4,7 +4,7 @@ mode_desc="Select and use the packages for the linux-hardened kernel"
 
 # Kernel versions for hardened packages
 pkgrel="1"
-kernel_version="4.13.3.a-1"
+kernel_version="4.13.4.a-1"
 
 # Kernel version for GIT packages
 pkgrel_git="${pkgrel}"
