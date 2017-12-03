878ae99e277ef9cd1c7c1f0fe4f3a223be7a13fd
Jesus Alvarez <jeezusjr@gmail.com>
2017-12-03 11:04:11 -0800

    Update packages
    Closes #190

--- src/kernels/linux-lts.sh
+++ src/kernels/linux-lts.sh
@@ -4,7 +4,7 @@ mode_desc="Select and use the packages for the linux-lts kernel"
 
 # Kernel versions for LTS packages
 pkgrel="1"
-kernel_version="4.9.64-1"
+kernel_version="4.9.66-1"
 
 # Kernel version for GIT packages
 pkgrel_git="${pkgrel}"
