3ec41f698b7d600b13276ce0ea09a41f19f3fb5f
Jesus Alvarez <jeezusjr@gmail.com>
2018-01-18 16:30:47 -0800

    Update packages

--- src/kernels/linux-lts.sh
+++ src/kernels/linux-lts.sh
@@ -5,7 +5,7 @@ mode_desc="Select and use the packages for the linux-lts kernel"
 
 # Kernel versions for LTS packages
 pkgrel="1"
-kernel_version="4.9.76-1"
+kernel_version="4.9.77-1"
 
 # Kernel version for GIT packages
 pkgrel_git="${pkgrel}"
--- src/kernels/linux-zen.sh
+++ src/kernels/linux-zen.sh
@@ -5,7 +5,7 @@ mode_desc="Select and use the packages for the linux-zen kernel"
 
 # Kernel versions for default ZFS packages
 pkgrel="1"
-kernel_version="4.14.13-1"
+kernel_version="4.14.13-2"
 
 # Kernel version for GIT packages
 pkgrel_git="${pkgrel}"
