1342b9cbaa86916d2b4660b5b8ea144ce51583b4
Jesus Alvarez <jeezusjr@gmail.com>
2018-02-26 12:42:18 -0800

    Update packages
    Closes #229

--- src/kernels/linux-hardened.sh
+++ src/kernels/linux-hardened.sh
@@ -5,7 +5,7 @@ mode_desc="Select and use the packages for the linux-hardened kernel"
 
 # Kernel versions for hardened packages
 pkgrel="1"
-kernel_version="4.15.4.a-1"
+kernel_version="4.15.6.a-1"
 
 # Kernel version for GIT packages
 pkgrel_git="${pkgrel}"
