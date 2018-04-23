5993f8685d8d8b2990799f5a767a8535e27b7125
Jesus Alvarez <jeezusjr@gmail.com>
2018-04-23 10:49:22 -0700

    Update packages

--- src/kernels/linux-vfio.sh
+++ src/kernels/linux-vfio.sh
@@ -5,7 +5,7 @@ mode_desc="Select and use the packages for the linux-vfio kernel"
 
 # Kernel versions for default ZFS packages
 pkgrel="1"
-kernel_version="4.15.15-1"
+kernel_version="4.16.3-1"
 
 # Kernel version for GIT packages
 pkgrel_git="${pkgrel}"
