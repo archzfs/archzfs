adef3c82f5f1d31be35ae68700230950ee54ec79
Jan Houben <jan@et.tc>
2019-03-05 22:01:10 -0500

    Disable zfs-rc for linux kernel

--- src/kernels/linux.sh
+++ src/kernels/linux.sh
@@ -74,23 +74,24 @@ update_linux_pkgbuilds() {
     zfs_makedepends="\"${spl_pkgname}-headers\""
 }
 
-update_linux_rc_pkgbuilds() {
-    get_kernel_options
-    pkg_list=("zfs-linux-rc")
-    archzfs_package_group="archzfs-linux-rc"
-    zfs_pkgver=${zol_rc_version/-/_}
-    zfs_pkgrel=${pkgrel_rc}
-    zfs_conflicts="'zfs-linux' 'zfs-linux-git' 'spl-linux'"
-    spl_pkgname=""
-    zfs_pkgname="zfs-linux-rc"
-    zfs_utils_pkgname="zfs-utils-rc=\${_zfsver}"
-    zfs_src_hash=${zfs_rc_src_hash}
-    zfs_makedepends="\"python\""
-    # Paths are relative to build.sh
-    zfs_pkgbuild_path="packages/${kernel_name}/${zfs_pkgname}"
-    zfs_src_target="https://github.com/zfsonlinux/zfs/releases/download/zfs-\${_zfsver/_/-}/zfs-\${_zfsver/_/-}.tar.gz"
-    zfs_workdir="\${srcdir}/zfs-\${_zfsver/_rc*/}"
-}
+
+# update_linux_rc_pkgbuilds() {
+#     get_kernel_options
+#     pkg_list=("zfs-linux-rc")
+#     archzfs_package_group="archzfs-linux-rc"
+#     zfs_pkgver=${zol_rc_version/-/_}
+#     zfs_pkgrel=${pkgrel_rc}
+#     zfs_conflicts="'zfs-linux' 'zfs-linux-git' 'spl-linux'"
+#     spl_pkgname=""
+#     zfs_pkgname="zfs-linux-rc"
+#     zfs_utils_pkgname="zfs-utils-rc=\${_zfsver}"
+#     zfs_src_hash=${zfs_rc_src_hash}
+#     zfs_makedepends="\"python\""
+#     # Paths are relative to build.sh
+#     zfs_pkgbuild_path="packages/${kernel_name}/${zfs_pkgname}"
+#     zfs_src_target="https://github.com/zfsonlinux/zfs/releases/download/zfs-\${_zfsver/_/-}/zfs-\${_zfsver/_/-}.tar.gz"
+#     zfs_workdir="\${srcdir}/zfs-\${_zfsver/_rc*/}"
+# }
 
 update_linux_git_pkgbuilds() {
     get_kernel_options
