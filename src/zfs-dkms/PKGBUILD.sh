c3b57f943d5a89f28075dbb4526baa2b5475c8f4
Jan Houben <jan@et.tc>
2019-09-20 08:59:30 -0400

    Add Linux 5.3 Patches

--- src/zfs-dkms/PKGBUILD.sh
+++ src/zfs-dkms/PKGBUILD.sh
@@ -9,9 +9,15 @@ pkgver=${zfs_pkgver}
 pkgrel=${zfs_pkgrel}
 makedepends=(${zfs_makedepends})
 arch=("x86_64")
-url="http://zfsonlinux.org/"
-source=("${zfs_src_target}")
-sha256sums=("${zfs_src_hash}")
+url="https://zfsonlinux.org/"
+source=("${zfs_src_target}"
+        "linux-5.3-compat-rw_semaphore-owner.patch"
+        "linux-5.3-compat-retire-rw_tryupgrade.patch"
+        "linux-5.3-compat-Makefile-subdir-m-no-longer-supported.patch")
+sha256sums=("${zfs_src_hash}"
+            "c65c950abda42fb91fb99c6c916a50720a522c53e01a872f9310a4719bae9e2a"
+            "19f798a29c00874874751880f1146c5849b8ebdb6233d8ae923f9fdd4661de19"
+            "6c4627875dd1724f64a196ea584812c99635897dc31cb23641f308770289059a")
 license=("CDDL")
 depends=("${zfs_utils_pkgname}" "lsb-release" "dkms")
 provides=("zfs" "zfs-headers" "spl" "spl-headers")
@@ -37,5 +43,9 @@ package() {
 
 
 EOF
+
+if [[ ! ${archzfs_package_group} =~ -git$ ]] && [[ ! ${archzfs_package_group} =~ -rc$ ]]; then
+    sed -i "/^build()/i prepare() {\n    cd \"${zfs_workdir}\"\n    patch -Np1 -i \${srcdir}/linux-5.3-compat-rw_semaphore-owner.patch\n    patch -Np1 -i \${srcdir}/linux-5.3-compat-retire-rw_tryupgrade.patch\n    patch -Np1 -i \${srcdir}/linux-5.3-compat-Makefile-subdir-m-no-longer-supported.patch\n}" ${zfs_dkms_pkgbuild_path}/PKGBUILD
+fi
 
 pkgbuild_cleanup "${zfs_dkms_pkgbuild_path}/PKGBUILD"
--- src/zfs/PKGBUILD.sh
+++ src/zfs/PKGBUILD.sh
@@ -14,9 +14,15 @@ pkgver="\${_zfsver}_\$(echo \${_kernelver} | sed s/-/./g)"
 pkgrel=${zfs_pkgrel}
 makedepends=(${linux_headers_depends} ${zfs_makedepends})
 arch=("x86_64")
-url="http://zfsonlinux.org/"
-source=("${zfs_src_target}")
-sha256sums=("${zfs_src_hash}")
+url="https://zfsonlinux.org/"
+source=("${zfs_src_target}"
+        "linux-5.3-compat-rw_semaphore-owner.patch"
+        "linux-5.3-compat-retire-rw_tryupgrade.patch"
+        "linux-5.3-compat-Makefile-subdir-m-no-longer-supported.patch")
+sha256sums=("${zfs_src_hash}"
+            "c65c950abda42fb91fb99c6c916a50720a522c53e01a872f9310a4719bae9e2a"
+            "19f798a29c00874874751880f1146c5849b8ebdb6233d8ae923f9fdd4661de19"
+            "6c4627875dd1724f64a196ea584812c99635897dc31cb23641f308770289059a")
 license=("CDDL")
 depends=("kmod" "${zfs_utils_pkgname}" ${linux_depends})
 
@@ -25,7 +31,7 @@ build() {
     ./autogen.sh
     ./configure --prefix=/usr --sysconfdir=/etc --sbindir=/usr/bin --libdir=/usr/lib \\
                 --datadir=/usr/share --includedir=/usr/include --with-udevdir=/lib/udev \\
-                --libexecdir=/usr/lib/zfs-\${zfsver} --with-config=kernel \\
+                --libexecdir=/usr/lib/zfs-\${_zfsver} --with-config=kernel \\
                 --with-linux=/usr/lib/modules/\${_extramodules}/build \\
                 --with-linux-obj=/usr/lib/modules/\${_extramodules}/build
     make
@@ -62,5 +68,9 @@ package_${zfs_pkgname}-headers() {
 }
 
 EOF
+
+if [[ ! ${archzfs_package_group} =~ -git$ ]] && [[ ! ${archzfs_package_group} =~ -rc$ ]]; then
+    sed -i "/^build()/i prepare() {\n    cd \"${zfs_workdir}\"\n    patch -Np1 -i \${srcdir}/linux-5.3-compat-rw_semaphore-owner.patch\n    patch -Np1 -i \${srcdir}/linux-5.3-compat-retire-rw_tryupgrade.patch\n    patch -Np1 -i \${srcdir}/linux-5.3-compat-Makefile-subdir-m-no-longer-supported.patch\n}" ${zfs_pkgbuild_path}/PKGBUILD
+fi
 
 pkgbuild_cleanup "${zfs_pkgbuild_path}/PKGBUILD"
