d2bfe4ec92e4b72887c64abc75f02537375c46c0
Jan Houben <jan@et.tc>
2019-09-27 00:42:56 -0400

    Update zfs version to 0.8.2

--- conf.sh
+++ conf.sh
@@ -2,11 +2,11 @@
 #
 # FIXME: reset all kernel configs set to pkgrel=1 when this changes
 #
-zol_version="0.8.1"
+zol_version="0.8.2"
 zol_rc_version="0.8.0-rc5"
 
 # The ZOL source hashes are from zfsonlinux.org
-zfs_src_hash="0af79fde44b7b8ecb94d5166ce2e4fff7409c20ed874c2d759db92909e6c2799"
+zfs_src_hash="47608e257c8ecebb918014ef1da6172c3a45d990885891af18e80f5cc28beab8"
 zfs_rc_src_hash="c5dc91e3efb7555c6c1846cf89fd4cfb0952271a2900434e697f2b7397ce9b16"
 
 zfs_initcpio_install_hash="29a8a6d76fff01b71ef1990526785405d9c9410bdea417b08b56107210d00b10"
