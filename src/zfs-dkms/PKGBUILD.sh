fa309f47be4773c3516f43da48c5d40f58493336
Joe Groocock <me@frebib.net>
2020-02-29 11:02:46 +0000

    Add Linux 5.5 compat patch
    Upstream fix: https://github.com/openzfs/zfs/pull/10072

--- src/zfs-dkms/PKGBUILD.sh
+++ src/zfs-dkms/PKGBUILD.sh
@@ -10,8 +10,10 @@ pkgrel=${zfs_pkgrel}
 makedepends=(${zfs_makedepends})
 arch=("x86_64")
 url="https://zfsonlinux.org/"
-source=("${zfs_src_target}")
-sha256sums=("${zfs_src_hash}")
+source=("${zfs_src_target}"
+        "linux-5.5-compat-blkg_tryget.patch")
+sha256sums=("${zfs_src_hash}"
+            "daae58460243c45c2c7505b1d88dcb299ea7d92bcf3f41d2d30bc213000bb1da")
 license=("CDDL")
 depends=("${zfs_utils_pkgname}" "lsb-release" "dkms")
 provides=("zfs" "zfs-headers" "spl" "spl-headers")
@@ -37,5 +39,9 @@ package() {
 
 
 EOF
+
+if [[ ! ${archzfs_package_group} =~ -git$ ]] && [[ ! ${archzfs_package_group} =~ -rc$ ]]; then
+    sed -E -i "/^build()/i prepare() {\n    cd \"${zfs_workdir}\"\n    patch -Np1 -i \${srcdir}/linux-5.5-compat-blkg_tryget.patch\n}" ${zfs_dkms_pkgbuild_path}/PKGBUILD
+fi
 
 pkgbuild_cleanup "${zfs_dkms_pkgbuild_path}/PKGBUILD"
