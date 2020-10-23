3c2fa67573fb18b0291f1dfdc27655354b1482b7
Jan Houben <jan@et.tc>
2020-10-23 16:27:24 +0200

    Update zfs-rc version to 2.0.0-rc4

--- conf.sh
+++ conf.sh
@@ -3,11 +3,11 @@
 # FIXME: reset all kernel configs set to pkgrel=1 when this changes
 #
 zol_version="0.8.5"
-zol_rc_version="2.0.0-rc3"
+zol_rc_version="2.0.0-rc4"
 
 # The ZOL source hashes are from zfsonlinux.org
 zfs_src_hash="dbb41d6b9c606a34ac93f4c19069fd6806ceeacb558f834f8a70755dadb7cd3d"
-zfs_rc_src_hash="d06ef8baa44a302ff28c6860e4ff1bf454c617198f755dbbce5d46d3bb7bca7b"
+zfs_rc_src_hash="d89838d93b4429de98075de1a7fcac4b22673cedab4e9b109594f9f8c2456c51"
 
 zfs_initcpio_install_hash="29a8a6d76fff01b71ef1990526785405d9c9410bdea417b08b56107210d00b10"
 zfs_initcpio_hook_hash="446a4793cbb8ebf5196ef5ed60c372096c161c48bf714eb053f619791a7042f9"
