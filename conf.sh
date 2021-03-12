82f77442fa232ad61365cfe00ac116b17af049c4
Jan Houben <jan@et.tc>
2021-03-12 17:31:43 +0100

    Update zfs version to 2.0.4

--- conf.sh
+++ conf.sh
@@ -2,11 +2,11 @@
 #
 # FIXME: reset all kernel configs set to pkgrel=1 when this changes
 #
-zol_version="2.0.3"
+zol_version="2.0.4"
 zol_rc_version="2.0.0-rc7"
 
 # The ZOL source hashes are from zfsonlinux.org
-zfs_src_hash="0694f64aa76a3a0a506e20e99b49102c3cb68bef63cb0f0154e50befc387e539"
+zfs_src_hash="7d1344c5433b91823f02c2e40b33d181fa6faf286bea5591f4b1965f23d45f6c"
 zfs_rc_src_hash="640fde2b678040c9511172bf3b7ffc444d8e8daa1d738f4687f1ad92d6d01e7b"
 
 zfs_initcpio_install_hash="29a8a6d76fff01b71ef1990526785405d9c9410bdea417b08b56107210d00b10"
