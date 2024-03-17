18e617c824ad4b3c40eb89b087b0d46f1fdf0647
Jan Houben <jan@et.tc>
2024-03-17 14:19:05 -0400

    Add Linux 6.8 and Version Enforce Patch

--- src/zfs/PKGBUILD.sh
+++ src/zfs/PKGBUILD.sh
@@ -16,8 +16,8 @@ pkgrel=${zfs_pkgrel}
 makedepends=(${linux_headers_depends} ${zfs_makedepends})
 arch=("x86_64")
 url="https://openzfs.org/"
-source=("${zfs_src_target}")
-sha256sums=("${zfs_src_hash}")
+source=("${zfs_src_target}" "enforce-kernel-max-version.patch" "linux-6.8-compat.patch" "kernel-6.8-meta.patch")
+sha256sums=("${zfs_src_hash}" "c5a9f546638c706844d5aff99f40366db1684679c3318d3a4093e0746748a711" "b875c877069a4c75c7b2b4b22d048e66f415b86f862ef6b3b83d3524694cc973" "1bc3b2e79e481b1bf41e78f9d142de8e97326288ecdc97f8db65496b7c4fd63b")
 license=("CDDL")
 depends=("kmod" "${zfs_utils_pkgname}" ${linux_depends})
 
@@ -61,5 +61,9 @@ package_${zfs_pkgname}-headers() {
 }
 
 EOF
+
+if [[ ! ${archzfs_package_group} =~ -rc$ ]] && [[ "${mode_name}" != "lts" ]]; then
+    sed -E -i "/^build()/i prepare() {\n    cd \"${zfs_workdir}\"\n    patch -Np1 -i \${srcdir}/enforce-kernel-max-version.patch\n    patch -Np1 -i \${srcdir}/linux-6.8-compat.patch\n    patch -Np1 -i \${srcdir}/kernel-6.8-meta.patch\n}" ${zfs_pkgbuild_path}/PKGBUILD
+fi
 
 pkgbuild_cleanup "${zfs_pkgbuild_path}/PKGBUILD"
