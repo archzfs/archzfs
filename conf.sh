e596746664a51f4710b4cf4e2b38ef7340994e2d
Pavan Rikhi <pavan.rikhi@gmail.com>
2024-10-04 20:04:11 -0400

    Bump OpenZFS RC Version to 2.3.0-rc1

--- conf.sh
+++ conf.sh
@@ -3,11 +3,11 @@
 # FIXME: reset all kernel configs set to pkgrel=1 when this changes
 #
 openzfs_version="2.2.6"
-openzfs_rc_version="2.1.0-rc8"
+openzfs_rc_version="2.3.0-rc1"
 
 # The OpenZFS source hashes are from github.com/openzfs/zfs/releases
 zfs_src_hash="c92e02103ac5dd77bf01d7209eabdca55c7b3356aa747bb2357ec4222652a2a7"
-zfs_rc_src_hash="8627702ac841d38d5211001c76937e4097719c268b110e8836c0da195618fad2"
+zfs_rc_src_hash="a74b54788640629b426bd3ab3f2484e62c4620e899c92096bc8ef5fed1360a0a"
 
 zfs_initcpio_install_hash="d19476c6a599ebe3415680b908412c8f19315246637b3a61e811e2e0961aea78"
 zfs_initcpio_hook_hash="569089e5c539097457a044ee8e7ab9b979dec48f69760f993a6648ee0f21c222"
